44f97178e6b32c293c47a33b627ed75092dec977
leemj0948 <ljo094822@naver.com>
2023-05-31 11:23:47 +0900

    500error

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
@@ -16,27 +16,27 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <Image
+            {/* <Image
               src="/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
               height={24}
               priority
-            />
+            /> */}
           </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
+        {/* <Image
           className={styles.logo}
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
-        />
+        /> */}
       </div>
 
       <div className={styles.grid}>
