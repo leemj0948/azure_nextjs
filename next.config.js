9af9b5d7af6e21d027d1ca5b7d9973a2fcf332f7
leemj0948 <ljo094822@naver.com>
2023-05-31 10:28:20 +0900

    next.config.js setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
 const nextConfig = {}
 
 module.exports = nextConfig
